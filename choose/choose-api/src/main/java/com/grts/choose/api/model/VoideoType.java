4d5bb68be540856778453ae75415ecc9fdd2ff1b
huaiting11 <791897366@qq>
2019-09-10 22:09:20 +0800

    批量上传

---
+++ choose/choose-api/src/main/java/com/grts/choose/api/model/VoideoType.java
@@ -0,0 +1,22 @@
+package com.grts.choose.api.model;
+
+public class VoideoType {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- choose/choose-manager-web/src/main/java/com/grts/choose/managerweb/controller/videoController.java
+++ choose/choose-manager-web/src/main/java/com/grts/choose/managerweb/controller/videoController.java
@@ -1,7 +1,14 @@
 package com.grts.choose.managerweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("video")
@@ -10,6 +17,39 @@ public class videoController {
     public String index(){
         return "manager/videoList";
 
+    }
+    @RequestMapping("uploadVideo")
+    public String uploadVideo(@RequestParam("file") MultipartFile[] multipartFile,String carrId){
+        List<String> oNames = new ArrayList<String>();
+        List<String> fNames = new ArrayList<String>();
+        List<String> carr = new ArrayList<String>();
+        carr.add(carrId);
+        for (int i = 0; i < multipartFile.length; i++) {
+            MultipartFile file = multipartFile[i];
+            if (file.getSize()==0) {
+                continue;
+            }
+            String oName = file.getOriginalFilename();
+            oNames.add(oName);
+            try {
+                File path = new File(ResourceUtils.getURL("classpath:").getPath());
+                File upload = new File(path.getAbsolutePath(), "static/video/");
+                if (!upload.exists()) upload.mkdirs();
+                String uploadPath = upload + File.separator + oName;
+                File file1 = new File(uploadPath);
+                file1.mkdirs();
+                file.transferTo(file1);
+                fNames.add(file1.getName());
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+        List<List<String>> result = new ArrayList<List<String>>();
+        result.add(carr);
+        result.add(oNames);
+        result.add(fNames);
+        return "manager/videoList";
     }
 
 }
